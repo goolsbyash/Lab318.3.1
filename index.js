c38fd63ffcebf178ef753f36741554cc4a6d6290
goolsbyash <goolsbyashley015@gmail.com>
2024-01-09 16:00:59 -0500

    Started creating GET and POST routes for /comments

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const users = require("./routes/users");
 const posts = require("./routes/posts");
+const comments = require("./routes/comments");
 
 const error = require("./utilities/error");
 
@@ -55,6 +56,7 @@ app.use("/api", function (req, res, next) {
 // Use our Routes
 app.use("/api/users", users);
 app.use("/api/posts", posts);
+app.use("/api/comments", comments);
 
 // Adding some HATEOAS links.
 app.get("/", (req, res) => {
---
+++ routes/comments.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+
+const comments = require("../data/comments");
+const error = require("../utilities/error");
+
+router
+  .route("/")
+  .get((req, res) => {
+    const links = [
+      {
+        href: "comments/:id",
+        rel: ":id",
+        type: "GET",
+      },
+    ];
+
+    res.json({ comments, links });
+  })
+  .post((req, res) => {
+    if (req.body.userId && req.body.id && req.body.postId && req.body.body) {
+      const comment = {
+        id: comments[comments.length - 1].id + 1,
+        userId: req.body.userId,
+        postId: req.body.postId,
+        body: req.body.body,
+      };
+
+      comments.push(comment);
+      res.json(comments[comments.length - 1]);
+    } else next(error(400, "Insufficient Data"));
+  });
+
+module.exports = router;
